ba2cdcc7e8571c7d270d93b846f47b166bcc883b
Bruno Teixeira <elielsonbrunno@gmail.com>
2024-10-17 16:31:23 -0400

    feat :  add JavaScript and CSS styles.

--- script.js
+++ script.js
@@ -13,10 +13,10 @@ function toggleMode() {
     if(html.classList.contains('light')) {
   // se tiver light mode, adicionar a imagem light.
 
-      img.setAttribute('src','./avatar-light.png')
+      img.setAttribute("src", "./assets/avatar-light.png")
     } else {
       // se tiver sem light mode, manter a imagem normal
-      img.setAttribute('src', './avatar.png')
+      img.setAttribute("src", "./assets/avatar.png")
       }
 
   }
